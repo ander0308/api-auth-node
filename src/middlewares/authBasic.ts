4281e67e5f46846ff2015e11d3c7332229e38107
Anderson Santana <ander0308@yahoo.com.br>
2023-02-06 14:28:29 -0300

    feat: create function token with JWT Baerer

--- src/middlewares/authBasic.ts
+++ src/middlewares/authBasic.ts
@@ -1,9 +1,5 @@
 import { Request, Response, NextFunction} from 'express'
-import JWT from 'jsonwebtoken'
-import dotenv from 'dotenv'
 import { User } from '../models/User'
-
-dotenv.config()
 
 export const Auth = {
     private: async ( req: Request, res: Response, next: NextFunction) => {
@@ -11,16 +7,19 @@ export const Auth = {
         
         // Fazer verificação de auth
         if(req.headers.authorization) {
-            const [authType, token] = req.headers.authorization.split(' ')
-            if(authType === 'Bearer') {
-                try {
-                    JWT.verify(
-                        token,
-                        process.env.JWT_SECRET_KEY as string
-                    )
-                    success = true
-                } catch(error) {
+            let hash: string = req.headers.authorization.substring(6)
+            let decoded: string = Buffer.from(hash, 'base64').toString()
+            let data: string[] = decoded.split(':')
 
+            if(data.length === 2) {
+                let hasUser = await User.findOne({
+                    where: {
+                        email: data[0],
+                        password: data[1]
+                    }
+                })
+                if(hasUser){
+                    success = true
                 }
             }
         }
